ff4ac7c77ddb18b72e380f2530348ff1a58cb5f4
fasill <fasilhawultie19@gmail.com>
2024-08-01 15:09:33 +0300

    feat: ContactForm added

---
+++ ContactForm/src/App.tsx
@@ -0,0 +1,15 @@
+
+import MyForm from './pages/MyForm';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <MyForm/>
+    </div>
+  )
+}
+
+export default App
